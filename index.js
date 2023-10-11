d97e59f7280ca80e291be1208ddd337fce604b8c
jzip219 <81497216+jzip219@users.noreply.github.com>
2023-10-11 17:22:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ async function float32array()
 
   var x = new float32array(1,11)
 
-  x[0] = document.getElementById('box1
+  x[0] = document.getElementById('box1').value;
+  x[1] = document.getElementById('box1').value;
+  x[2] = document.getElementById('box1').value;
+  x[3] = document.getElementById('box1').value;
+  x[4] = document.getElementById('box1').value;
+  x[5] = document.getElementById('box1').value;
+  x[6] = document.getElementById('box1').value;
+  x[7] = document.getElementById('box1').value;
+  x[8] = document.getElementById('box1').value;
+  x[9] = document.getElementById('box1').value;
+  x[10] = document.getElementById('box1').value;
 
 let tensorX = new onnx.Tensor(x, float32, [1,11]);
 
 let session = new onnx.InferenceSession();
-await session.loadModel("./XGB_Winedata.py")
+await session.loadModel("./xgb_deployment.onnx");
+let outputMap = await session.run([)
