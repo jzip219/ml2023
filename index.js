bba5ec8de902da5787f7932fece12550fa1b1c09
jzip219 <81497216+jzip219@users.noreply.github.com>
2023-10-11 17:20:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+async function float32array()
+
+  var x = new float32array(1,11)
+
+  x[0] = document.getElementById('box1
+
+let tensorX = new onnx.Tensor(x, float32, [1,11]);
+
+let session = new onnx.InferenceSession();
+await session.loadModel("./XGB_Winedata.py")
