1da02d27f6e40a8987984f218562a234623e6883
jzip219 <81497216+jzip219@users.noreply.github.com>
2023-12-06 14:29:25 -0600

    Add files via upload

---
+++ ITS520-FINALPROJECT/cnnet.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+'''
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.cn1 = nn.Conv2d(1, 16, 3, 1)
+        self.cn2 = nn.Conv2d(16, 32, 3, 1)
+        self.dp1 = nn.Dropout2d(0.10)
+        self.dp2 = nn.Dropout2d(0.25)
+        self.fc1 = nn.Linear(4608, 64) # 4608 is basically 12 X 12 X 32
+        self.fc2 = nn.Linear(64, 10)
+ 
+    def forward(self, x):
+        x = self.cn1(x)
+        x = F.relu(x)
+        x = self.cn2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dp1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dp2(x)
+        x = self.fc2(x)
+        op = F.log_softmax(x, dim=1)
+        return op
+    
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+'''
